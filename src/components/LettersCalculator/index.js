456200b59c7fa7d2459613bb6aa1cb9536b26fff
tukaramgoud <tukaramgoud2804.com>
2024-03-28 20:08:34 +0000

    adding functionality JS

--- src/components/LettersCalculator/index.js
+++ src/components/LettersCalculator/index.js
@@ -3,30 +3,39 @@ import {Component} from 'react'
 import './index.css'
 
 class LettersCalculator extends Component {
+  state = {count: 0}
+
+  changeOfInput = event => {
+    this.setState({count: event.target.value.length})
+  }
+
   render() {
+    const {count} = this.state
+
     return (
       <div className="main-container">
         <div className="text-container">
-          <h1 className="main-heading">Calculate letters that you Entered</h1>
+          <h1 className="main-heading">Calculate the Letters you enter</h1>
           <div className="input-container">
             <label htmlFor="inputEl" className="label-styling">
-              Enter Your Phrase
+              Enter the phrase
             </label>
             <input
               type="text"
               id="inputEl"
               className="user-input"
-              placeholder="Enter the Phrase"
+              placeholder="Enter the phrase"
+              onChange={this.changeOfInput}
             />
           </div>
           <div className="para-container">
-            <p className="style-paraCount">No.of Letters : 0</p>
+            <p className="style-paraCount">No.of letters: {count}</p>
           </div>
         </div>
         <div className="image-container">
           <img
             src="https://assets.ccbp.in/frontend/react-js/stop-watch-with-calculator-img.png"
-            alt="letter calculator"
+            alt="letters calculator"
             className="image-sizing"
           />
         </div>
